b0eb4bb08ee3045827565063d6991e6fe225d390
Rebertt Rodrigues <joaorebertt0219@gmail.com>
2025-03-13 02:20:54 -0300

    Update vps-checker.ts

--- src/jobs/usecases/vps-checker.ts
+++ src/jobs/usecases/vps-checker.ts
@@ -22,27 +22,28 @@ export class VpsChecker {
                             "memInfo": {
                                 "totalMemMb": '$(free -m | awk '/Mem:/ {print $2}')',
                                 "usedMemMb": '$(free -m | awk '/Mem:/ {print $3}')',
-                                "freeMemMb": '$(free -m | awk '/Mem:/ {print $4}')',
-                                "usedMemPercentage": '$(free | awk '/Mem:/ {printf "%.2f", $3/$2 * 100}')',
-                                "freeMemPercentage": '$(free | awk '/Mem:/ {printf "%.2f", $4/$2 * 100}')'
+                                "freeMemMb": '$(free -m | awk '/Mem:/ {print $7}')',  # Corrigido para "available"
+                                "usedMemPercentage": '$(free -m | awk '/Mem:/ {printf "%.2f", ($2-$7)/$2 * 100}')',
+                                "freeMemPercentage": '$(free -m | awk '/Mem:/ {printf "%.2f", $7/$2 * 100}')'
                             },
                             "diskInfo": {
-                                "totalGb": "'$(df -h / | awk 'NR==2 {print $2}' | sed 's/G//')'",
-                                "usedGb": "'$(df -h / | awk 'NR==2 {print $3}' | sed 's/G//')'",
-                                "freeGb": "'$(df -h / | awk 'NR==2 {print $4}' | sed 's/G//')'",
+                                "totalGb": "'$(df -BG / | awk 'NR==2 {print $2}' | sed 's/G//')'",
+                                "usedGb": "'$(df -BG / | awk 'NR==2 {print $3}' | sed 's/G//')'",
+                                "freeGb": "'$(df -BG / | awk 'NR==2 {print $4}' | sed 's/G//')'",
                                 "usedPercentage": "'$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')'",
                                 "freePercentage": "'$(awk 'BEGIN {printf "%.1f", 100 - ('$(df / | awk 'NR==2 {print $5}' | sed 's/%//')') }')'"
                             },
                             "cpuInfo": {
-                                "usedPercentage": '$(top -bn1 | grep '%Cpu' | awk '{print 100 - $8}')',
+                                "usedPercentage": '$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')',
                                 "count": '$(nproc)',
-                                "loadavg": ['$(awk '{print $1","$2","$3}' /proc/loadavg)']
+                                "loadavg": ['$(cat /proc/loadavg | awk '{print $1","$2","$3}')']
                             }
                         }
                         }
                     }
                 }'
             `
+
 
             await this.sshService.execute(command, data.monitorId, data.vpsCredentials)
         } catch (e) {
