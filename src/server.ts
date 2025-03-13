abe5c8049173fe9e46409a7cf551bce660c03cd6
Rebertt Rodrigues <joaorebertt0219@gmail.com>
2025-03-13 14:10:49 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,18 @@
-import { app } from '@/http/app'
-import { env } from '@/env'
-import {QueueService} from "@/services/queue-service";
-import {VpsMonitor} from "@/cron/usecases/vps-monitor";
-import cron from "node-cron";
-import {manageWorkers} from "@/jobs/workers/worker-manager";
-import {vpsCheckerProcessor} from "@/jobs/processors/vps-checker-processor";
+import { app } from '@/http/app';
+import { env } from '@/env';
+import { manageWorkers } from "@/jobs/workers/worker-manager";
+import { vpsCheckerProcessor } from "@/jobs/processors/vps-checker-processor";
+import "@/cron"
 
-const queueService = new QueueService()
-const vpsMonitor = new VpsMonitor(queueService)
+console.log("🚀 Scheduler e Listener iniciados...");
 
-const scheduler = async () => {
-  await vpsMonitor.execute()
-}
-
-cron.schedule('*/5 * * * * *', scheduler)
-
-console.log("🚀 Scheduler iniciado...")
-
-manageWorkers("vps-monitoring", vpsCheckerProcessor)
-
-console.log("🚀 Listener iniciado...")
+manageWorkers("vps-monitoring", vpsCheckerProcessor);
 
 app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
-  const baseHost =
-    env.NODE_ENV === 'dev' ? `http://localhost:${env.PORT}` : `${env.HOST}`
+    const baseHost =
+        env.NODE_ENV === 'dev' ? `http://localhost:${env.PORT}` : `${env.HOST}`;
 
-  console.log('🚀 HTTP Server is Running:')
-  console.log(`- API: ${baseHost}/`)
-  console.log(`- Swagger: ${baseHost}/docs`)
-})
+    console.log('🚀 HTTP Server is Running:');
+    console.log(`- API: ${baseHost}/`);
+    console.log(`- Swagger: ${baseHost}/docs`);
+});
