00af3c7df3b9033be9aa93a32049fc2a0b6f6bb5
erosnoxx <garotoslixo@gmail.com>
2025-03-13 03:30:06 -0300

    feat: add create org + subscription beta

--- prisma/migrations/20250301020612_alter_command_column_to_jsonb/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `command` on the `SSHCommands` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "SSHCommands" DROP COLUMN "command",
-ADD COLUMN     "command" JSONB NOT NULL;
